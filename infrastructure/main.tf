2b95dace775b4f890c7b2a965a5a6994d0e4610a
Maycon Tanii <maycotanii.dev@gmail.com>
2025-04-06 20:42:56 -0300

    - remocao build lambda terraform

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -79,26 +79,3 @@ resource "aws_sqs_queue_policy" "resale_order_processor_queue_policy" {
 }
 EOF
 }
-
-resource "aws_iam_role" "lambda_exec" {
-  name = "lambda-exec-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "lambda.amazonaws.com"
-      }
-    }]
-  })
-}
-
-resource "aws_lambda_function" "resale_order_preparer_lambda" {
-  function_name = "resaleOrderPreparerLambda"
-  handler       = "index.handler"
-  runtime       = "nodejs18.x"
-  role          = aws_iam_role.lambda_exec.arn
-  filename      = "${path.module}/lambda/function.zip"
-  source_code_hash = filebase64sha256("${path.module}/lambda/function.zip")
-}
